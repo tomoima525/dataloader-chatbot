e3fe4c6d0a15b09e5af559d3f1f3cc39d0510ce4
Tomoaki Imai <tomoima525@gmail.com>
2023-05-02 17:25:15 -0700

    summerize test

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ async function main(): Promise<void> {
     model,
     'https://www.coindesk.com/business/2023/04/21/ether-erases-all-gains-from-shanghai-rally-as-bitcoin-crypto-prices-fall/',
   );
-  const question = "What's the cause of the price drop?";
+  const question = 'Summarize the article.';
   const res = await chain.call({
     question,
     chat_history: [],
